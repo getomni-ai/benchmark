f6947a90df105d81c508f4962fa109e03644e61b
Mark (Kailing) Ding <49173589+kailingding@users.noreply.github.com>
2025-01-20 18:31:32 -0800

    rename page

--- dashboard/pages/2_Test_Result.py
+++ dashboard/pages/2_Test_Result.py
@@ -5,7 +5,7 @@ from difflib import HtmlDiff
 from utils.style import SIDEBAR_STYLE
 
 
-st.set_page_config(page_title="Error Analysis", layout="wide")
+st.set_page_config(page_title="Test Results", layout="wide")
 st.markdown(SIDEBAR_STYLE, unsafe_allow_html=True)
 
 
